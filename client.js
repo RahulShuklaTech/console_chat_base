070d67cdd712795061b7fbb06e056d6aa8228e26
RahulShuklaTech <73771388+RahulShuklaTech@users.noreply.github.com>
2021-01-24 23:56:04 +0530

    Update client.js

--- client.js
+++ client.js
@@ -1,6 +1,20 @@
 const io = require("socket.io-client");
 const readline = require('readline');
 const rl = readline.createInterface({ input: process.stdin,  output: process.stdout });
-
-// write your code here
-
+rl.question("What is your name? ", answer => {
+  const socket = io('http://localhost:3000');   
+  const messagePrompt = () => {
+    rl.question(">", msg => {
+    console.log(`Sending message: "${msg}"`)
+    let ms = `${answer} says "${msg}"`
+    socket.emit('simple chat message',ms);
+    messagePrompt();
+    
+  })}
+  socket.on("connect", () => {
+    console.log("Sucessfully Connected to server");
+    messagePrompt();
+  });
+  socket.on("simple chat message", msg => console.log(msg))
+  socket.on("disconnect", () => console.log("Connection Lost.")) 
+});
